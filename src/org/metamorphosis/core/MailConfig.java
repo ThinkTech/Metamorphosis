1e7748e3c43890e39177e877fe88cf45961b030e
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2017-03-23 16:31:18 +0000

    improving the core system.

---
+++ src/org/metamorphosis/core/MailConfig.java
@@ -0,0 +1,61 @@
+package org.metamorphosis.core;
+
+import java.util.Properties;
+
+public class MailConfig {
+	
+	private String user ="ThinkTech.sn Info <info@thinktech.sn>";
+    private String password ="California2003";
+    Properties properties = new Properties();
+    
+    public MailConfig() {
+    	properties.put("mail.smtp.auth", "true");
+ 		properties.put("mail.smtp.starttls.enable", "true");
+ 		properties.put("mail.smtp.host", "smtp.thinktech.sn");
+ 		properties.put("mail.smtp.port", "25");
+    }
+    
+    public MailConfig(String user,String password,String host) {
+    	this.user = user;
+    	this.password = password;
+    	properties.put("mail.smtp.auth", "true");
+ 		properties.put("mail.smtp.starttls.enable", "true");
+ 		properties.put("mail.smtp.host", host);
+ 		properties.put("mail.smtp.port", "25");
+    }
+    
+    public MailConfig(String user,String password,String host,String port) {
+    	this.user = user;
+    	this.password = password;
+    	properties.put("mail.smtp.auth", "true");
+ 		properties.put("mail.smtp.starttls.enable", "true");
+ 		properties.put("mail.smtp.host", host);
+ 		properties.put("mail.smtp.port", port);
+    }
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Properties getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+    
+
+}
