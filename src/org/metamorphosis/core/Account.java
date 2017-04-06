b1f2ca32c01704807972e7ee15c1201b9439cf2f
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2017-04-06 15:34:25 +0000

    improving the core system.

--- src/org/metamorphosis/core/Account.java
+++ src/org/metamorphosis/core/Account.java
@@ -5,8 +5,6 @@ import java.util.Date;
 public class Account {
 
 	private Long id;
-	private String email;
-	private String password;
 	private String role;
 	private Structure structure = new Structure();
 	private Date createdOn;
@@ -17,18 +15,6 @@ public class Account {
 	}
 	public void setId(Long id) {
 		this.id = id;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
 	}
 	public String getRole() {
 		return role;
--- src/org/metamorphosis/core/User.java
+++ src/org/metamorphosis/core/User.java
@@ -8,6 +8,8 @@ public class User {
 	private Long id;
 	private String firstName;
 	private String lastName;
+	private String email;
+	private String password;
 	private List<Account> accounts = new ArrayList<Account>();
 	
 	public Long getId() {
@@ -27,6 +29,18 @@ public class User {
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	public List<Account> getAccounts() {
 		return accounts;
