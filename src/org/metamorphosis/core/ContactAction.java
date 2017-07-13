e25163cbe82bb04163f1fa270fbdf05cda8f2ec5
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2017-07-13 13:32:23 +0000

    improving the project.

--- src/org/metamorphosis/core/ContactAction.java
+++ src/org/metamorphosis/core/ContactAction.java
@@ -1,13 +1,18 @@
 package org.metamorphosis.core;
 
 @SuppressWarnings("serial")
-public class MailAction extends ActionSupport {
+public class ContactAction extends ActionSupport {
 
 	private Mail mail;
 	
 	public void sendMail() {
 		MailSender mailSender = new MailSender();
 		mailSender.sendMail(mail,true);		
+	}
+	
+	public void subscribe() {
+		System.out.println("mail address "+mail.getAddress());
+		System.out.println("subscribe done");
 	}
 
 	public Mail getMail() {
