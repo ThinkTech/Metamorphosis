f047709861fcc827cd52d61d54a36b5e63798a78
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2017-07-14 20:19:32 +0000

    improving the project.

--- src/org/metamorphosis/core/ContactAction.java
+++ src/org/metamorphosis/core/ContactAction.java
@@ -1,5 +1,10 @@
 package org.metamorphosis.core;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 @SuppressWarnings("serial")
 public class ContactAction extends ActionSupport {
 
@@ -11,6 +16,27 @@ public class ContactAction extends ActionSupport {
 	}
 	
 	public void subscribe() {
+		try {
+			Connection connection = getConnection();
+			PreparedStatement stmt = connection.prepareStatement("select * from newsletter where email = ?;");
+			stmt.setString(1, mail.getAddress());
+			ResultSet rs = stmt.executeQuery();
+			if(!rs.next()) {
+				stmt = connection.prepareStatement("insert into newsletter(email,structure_id) values(?,?);");
+				stmt.setString(1, mail.getAddress());
+				stmt.setInt(2, 1);
+				stmt.executeUpdate();
+			}
+			stmt.close();
+			connection.close();
+		}catch(Exception e) {
+			e.printStackTrace(System.out);
+		}
+	 }
+	
+	private Connection getConnection() throws Exception {
+		Class.forName("com.mysql.jdbc.Driver");  
+		return DriverManager.getConnection("jdbc:mysql://localhost/general","root","thinktech");
 	}
 
 	public Mail getMail() {
