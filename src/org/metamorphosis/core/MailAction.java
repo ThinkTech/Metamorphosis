1225e1963bf9d08dc5a344b99025c426cbce85d7
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2017-10-13 12:22:39 +0000

    improving the project.

---
+++ src/org/metamorphosis/core/MailAction.java
@@ -0,0 +1,22 @@
+package org.metamorphosis.core;
+
+
+@SuppressWarnings("serial")
+public class MailAction extends ActionSupport {
+
+	private Mail mail;
+	
+	public void sendMail() {
+		MailSender mailSender = new MailSender();
+		mailSender.sendMail(mail,true);		
+	}
+
+	public Mail getMail() {
+		return mail;
+	}
+
+	public void setMail(Mail mail) {
+		this.mail = mail;
+	}
+
+}
