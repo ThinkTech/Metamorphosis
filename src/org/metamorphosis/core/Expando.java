117afaac6ebb90b8c05d7570b99fdcb4ae7afbe5
ThinkTech <dev@thinktech.sn>
2018-07-04 20:38:58 +0000

    improving the project.

--- src/org/metamorphosis/core/Expando.java
+++ src/org/metamorphosis/core/Expando.java
@@ -1,21 +1,80 @@
 package org.metamorphosis.core;
 
-import java.util.Iterator;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
-public class Expando extends groovy.util.Expando{
+public class Expando extends groovy.util.Expando implements Map<Object,Object>{
 
+	private Map<Object,Object> map = new HashMap<Object,Object>();
+	
 	public Expando(){
 		super();
 	}
 	
-	@SuppressWarnings("rawtypes")
-	public Expando(Map row){
-		Iterator it = row.entrySet().iterator();
-	    while (it.hasNext()) {
-	        Map.Entry pair = (Map.Entry)it.next();
-	        setProperty(pair.getKey().toString(),pair.getValue());
-	    }
+	public Expando(Map<Object,Object> map){
+		this.map = map;
+	}
+
+	@Override
+	public int size() {
+		return map.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return map.isEmpty();
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		return map.containsKey(key);
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		return map.containsValue(value);
+	}
+
+	@Override
+	public Object get(Object key) {
+		return map.get(key);
+	}
+
+	@Override
+	public Object put(Object key, Object value) {
+		return map.put(key, value);
+	}
+
+	@Override
+	public Object remove(Object key) {
+		return map.remove(key);
+	}
+
+	@Override
+	public void putAll(Map<? extends Object, ? extends Object> m) {
+		map.putAll(m);
+	}
+
+	@Override
+	public void clear() {
+		map.clear();
+	}
+
+	@Override
+	public Set<Object> keySet() {
+		return map.keySet();
+	}
+
+	@Override
+	public Collection<Object> values() {
+		return map.values();
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<Object, Object>> entrySet() {
+		return map.entrySet();
 	}
 	
 }
