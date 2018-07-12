908b47ea91328aae032030f9dc5c794a14b3f777
ThinkTech <dev@thinktech.sn>
2018-07-12 12:16:10 +0000

    improving the project.

--- src/org/metamorphosis/core/Result.java
+++ src/org/metamorphosis/core/Result.java
@@ -1,14 +1,10 @@
 package org.metamorphosis.core;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Result {
 
 	private String name="success";
 	private String type="tiles";
 	private String value="";
-	private List<Parameter> parameters = new ArrayList<Parameter>();
 	
 	public String getName() {
 		return name;
@@ -27,14 +23,5 @@ public class Result {
 	}
 	public void setValue(String value) {
 		this.value = value;
-	}
-	public void addParameter(Parameter parameter) {
-		parameters.add(parameter);
-	}
-	public List<Parameter> getParameters() {
-		return parameters;
-	}
-	public void setParameters(List<Parameter> parameters) {
-		this.parameters = parameters;
 	}
 }
