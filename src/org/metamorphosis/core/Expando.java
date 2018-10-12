f71f399390292aca779d264a436a740f4e1aa097
thinktech <dev@thinktech.sn>
2018-10-12 12:54:14 +0000

    improving the project.

--- src/org/metamorphosis/core/Expando.java
+++ src/org/metamorphosis/core/Expando.java
@@ -1,86 +1,76 @@
 package org.metamorphosis.core;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+@SuppressWarnings("unchecked")
 public class Expando extends groovy.util.Expando implements Map<Object,Object> {
-
-	private Map<Object,Object> map = new HashMap<Object,Object>();
-	
-	public Expando() {
-		super();
-	}
-	
-	public Expando(Map<Object,Object> map) {
-		this.map = map;
-	}
 
 	@Override
 	public int size() {
-		return map.size();
+		return getProperties().size();
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return map.isEmpty();
+		return getProperties().isEmpty();
 	}
 
 	@Override
 	public boolean containsKey(Object key) {
-		return map.containsKey(key);
+		return getProperties().containsKey(key);
 	}
 
 	@Override
 	public boolean containsValue(Object value) {
-		return map.containsValue(value);
+		return getProperties().containsValue(value);
 	}
 
 	@Override
 	public Object get(Object key) {
-		return map.get(key);
+		return getProperties().get(key);
 	}
 
+	
 	@Override
 	public Object put(Object key, Object value) {
-		return map.put(key, value);
+		return getProperties().put(key, value);
 	}
 
 	@Override
 	public Object remove(Object key) {
-		return map.remove(key);
+		return getProperties().remove(key);
 	}
 
 	@Override
 	public void putAll(Map<? extends Object, ? extends Object> m) {
-		map.putAll(m);
+		getProperties().putAll(m);
 	}
 
 	@Override
 	public void clear() {
-		map.clear();
+		getProperties().clear();
 	}
 
 	@Override
 	public Set<Object> keySet() {
-		return map.keySet();
+		return getProperties().keySet();
 	}
 
 	@Override
 	public Collection<Object> values() {
-		return map.values();
+		return getProperties().values();
 	}
 
 	@Override
 	public Set<java.util.Map.Entry<Object, Object>> entrySet() {
-		return map.entrySet();
+		return getProperties().entrySet();
 	}
 	
 	@Override
 	public void setProperty(String property,Object newValue) {
 		super.setProperty(property, newValue);
-		map.put(property, newValue);
 	}
 	
 }
