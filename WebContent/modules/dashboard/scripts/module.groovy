fe1a791c3cfd87bc9c49b4b1294da70063d2348c
Thinktech <dev@thinktech.sn>
2019-06-04 10:11:27 +0100

    improving the project.

---
+++ WebContent/modules/dashboard/scripts/module.groovy
@@ -0,0 +1,12 @@
+class ModuleAction extends ActionSupport {
+                               
+      def String execute() {
+         println "hello"
+         SUCCESS        
+      }
+      
+      def test() {
+         println "test"        
+      }
+
+}
