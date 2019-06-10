22a59282c2e647e56bf6dd3a9dfd68496f353f0a
Thinktech <dev@thinktech.sn>
2019-06-10 12:56:15 +0100

    improving the project.

---
+++ src/org/metamorphosis/core/DynamicInvocationHandler.java
@@ -0,0 +1,27 @@
+package org.metamorphosis.core;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class DynamicInvocationHandler implements InvocationHandler {
+
+	private Object target;
+	
+	public DynamicInvocationHandler(Object target) {
+	    this.target = target;	
+	}
+	
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		return method.invoke(target, args);
+	}
+
+	public Object getTarget() {
+		return target;
+	}
+
+	public void setTarget(Object target) {
+		this.target = target;
+	}
+
+}
--- src/org/metamorphosis/core/Initializer.java
+++ src/org/metamorphosis/core/Initializer.java
@@ -2,7 +2,10 @@ package org.metamorphosis.core;
 
 import java.io.File;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Proxy;
 import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 import javax.servlet.FilterRegistration;
@@ -16,9 +19,11 @@ public class Initializer {
 	
 	private File folder;
 	private final ServletContext context;
+	private final Map<String,DynamicInvocationHandler> handlers;
 	
 	public Initializer(ServletContext context) {
 		this.context = context;
+		this.handlers = new HashMap<String,DynamicInvocationHandler>();
 		new ScriptManager(context);
 		context.setAttribute("path",context.getContextPath()+"/");
 	}
@@ -33,6 +38,7 @@ public class Initializer {
     		try {
 				File[] files = folder.listFiles();
 				if(files!=null) for(File file : files) register(file);
+				monitor(folder);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -57,7 +63,11 @@ public class Initializer {
 		String name = webServlet.name().trim().equals("")?object.getClass().getName():webServlet.name();
 		ServletRegistration registration = context.getServletRegistration(name);
 		if(registration==null) {
-			registration = context.addServlet(name, (Servlet) object);
+			DynamicInvocationHandler handler = new DynamicInvocationHandler(object);
+			Servlet servlet = (Servlet) Proxy.newProxyInstance(Servlet.class.getClassLoader(),
+                    new Class[] { Servlet.class },handler);
+			handlers.put(name, handler);
+			registration = context.addServlet(name,servlet);
 			if(webServlet.value().length>0)registration.addMapping(webServlet.value());
 			if(webServlet.urlPatterns().length>0)registration.addMapping(webServlet.urlPatterns());
 		}else {
@@ -70,12 +80,49 @@ public class Initializer {
 		String name = object.getClass().getName();
 		FilterRegistration registration = context.getFilterRegistration(name);
 		if(registration==null) {
-			registration = context.addFilter(name, (Filter) object);
+			DynamicInvocationHandler handler = new DynamicInvocationHandler(object);
+			Filter filter = (Filter) Proxy.newProxyInstance(Filter.class.getClassLoader(),
+                    new Class[] { Filter.class },handler);
+			handlers.put(name, handler);
+			registration = context.addFilter(name,filter);
 			if(webFilter.value().length>0) registration.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST,DispatcherType.FORWARD),true,webFilter.value());
 			if(webFilter.urlPatterns().length>0)registration.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST,DispatcherType.FORWARD),true,webFilter.urlPatterns());
 		}else {
 			String message = "The filter with the name " + name+" has already been registered. Please use a different name or package";
 			throw new RuntimeException(message);
+		}
+	}
+	
+	private void monitor(final File folder) {
+		String reload = System.getenv("metamorphosis.reload");
+		if("true".equals(reload)) {
+			new FileMonitor(folder).addListener(new FileAdapter() {
+				public void onFileCreated(String file) {
+				  File script = new File(folder+"/"+file);
+				   try {
+					   Object object = ScriptManager.getInstance().loadScript(script);
+					   Annotation[] annotations = object.getClass().getAnnotations();
+						for(Annotation annotation : annotations) {
+						   if(annotation instanceof WebServlet) {
+							   WebServlet webServlet = (WebServlet) annotation;
+							   String name = webServlet.name().trim().equals("")?object.getClass().getName():webServlet.name();
+							   DynamicInvocationHandler handler = handlers.get(name);
+							   if(handler!=null) handler.setTarget(object);
+								
+						   }
+						   else if(annotation instanceof WebFilter) {
+							   String name = object.getClass().getName();
+							   DynamicInvocationHandler handler = handlers.get(name);
+							   if(handler!=null) handler.setTarget(object);
+						   }
+						}
+				   } catch (Exception e) {
+					e.printStackTrace();
+				   }
+			    	
+				}
+				
+			}).monitor();
 		}
 	}
 	
