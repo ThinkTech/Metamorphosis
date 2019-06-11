00bd4f3cd3a6a6121bd3826d1ccef6e1845a3d75
Thinktech <dev@thinktech.sn>
2019-06-11 10:50:14 +0100

    improving the project.

--- WebContent/scripts/MyServlet.groovy
+++ WebContent/scripts/MyServlet.groovy
@@ -5,5 +5,10 @@ class MyServlet extends Servlet {
 		println "hello world from a groovy servlet"
 		json([status:1])
 	}
+	
+	def post() {
+	   	println "hello $request.name"
+		json([status:1])
+	}
 		
 }
